22805886ae3611e0e143a393475c05f35bd0c00f
GregoireW <24318548+GregoireW@users.noreply.github.com>
2024-09-02 11:35:47 +0200

    Add credHelpers to produce credentials

---
+++ packages/oci/hack/docker-credential-fake.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+process.stdin.setEncoding('utf8');
+
+let input = '';
+
+process.stdin.on('data', (chunk) => {
+  input += chunk;
+});
+
+process.stdin.on('end', () => {
+  try {
+    const firstParam = process.argv[2];
+
+    if (firstParam === 'get' && input === 'my-registry') {
+      const credentials = {
+        Username: 'username',
+        Secret: 'password'
+      };
+      console.log(JSON.stringify(credentials));
+    } else {
+      process.exit(-1);
+    }
+  } catch (err) {
+    console.error('Failed to parse input:', err);
+    process.exit(-1);
+  }
+});
